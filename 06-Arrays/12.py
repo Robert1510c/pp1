c79c8217b048d0ad4a3b5280c34d0dcdb0f1f090
s-121-27 <s-121-27@gumiforest.ae.krakow.pl>
2022-11-14 14:43:53 +0100

    Z lekcji

---
+++ 06-Arrays/12.py
@@ -0,0 +1,13 @@
+array=[
+    [2,5,4],
+    [9,0,3]
+]
+print(array)
+print(len(array),len(array[0]))
+print(array[0][1])
+print(array[1][2])
+print(array[1])
+print(array[0])
+print(array[0][-1],array[1][-1])
+for i in array:
+    print(i)
